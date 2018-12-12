73cd6fd0f633a6eb1d104dbc3dbbe1bc7329bee7
Kaeland Chatman <kaeland1@gmail.com>
2018-12-12 18:12:08 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,31 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
+var element = document.createElement('div');
+
+// We can set properties on this element:
+
+element.innerHTML = 'Hello, DOM!';
+element.style.backgroundColor = '#f9f9f9';
+
+// How to get an element to appear in the DOM:
+// You'll need to appead the Child node to an existing
+// DOM element.
+document.body.appendChild(element);
+
+element.style.textAlign = 'center';
+
+var ul = document.createElement('ul');
+
+for (let i = 0; i < 3; i++) {
+  let li = document.createElement('li');
+  li.innerHTML = (i + 1).toString();
+  ul.appendChild();
+};
+
+element.appendChild(ul);
+
+ul.style.textAlign = 'left';
+
+ul.removeChild(ul.querySelector('li:nth-child(2)'));
+
+ul.remove();
